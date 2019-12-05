a5fa4241af3d094a755c5298f237c16640372bd1
Erik Mai <emai003@wch133-31.cs.ucr.edu>
2019-12-04 17:33:23 -0800

    Rough queries

--- Phase3/sql/queries.sql
+++ Phase3/sql/queries.sql
@@ -21,18 +21,56 @@ string managerVar = "SELECT h.manager FROM Hotel h WHERE h.hID = hIDVar";
 INSERT INTO Request
 VALUES (reqIDrepairID, managerVar, repairID, requestDate, description);
 
-SELECT COUNT(*) 
-FROM Room r
-WHERE r.roomNo IN(
-	(SELECT r.roomNo
-	FROM Room r)
-	AND r.roomNo NOT IN
-	(SELECT r.roomNo
-	FROM Booking b, Room r
-	WHERE b.hotelId = r.hotelID AND b.roomNo = r.roomNo));   
+SELECT Total_Rooms - Booked_Rooms as Available_Rooms
+FROM ((SELECT r.roomNo as Total_Rooms
+	FROM Room r
+	WHERE r.hotelID = hotelID) AND
+	(SELECT r.roomNo as Booked_Rooms
+	  FROM Room r, Booking b
+	  WHERE r.hotelID = b.hotelID))
 
 SELECT COUNT(*)
 FROM Booking b, Room r
 WHERE b.hotelID = r.hotelID AND b.roomNo = r.roomNo;
+
+SELECT b.roomNo, b.roomType. b.price
+FROM Booking b
+LIMIT K
+ORDER BY b.price ASC;
+
+SELECT r.repairType, r.hotelID, r.roomNo
+FROM Repair r, MaintenanceCompany m
+WHERE r.mCompany = m.cmpID AND m.name = name;
+
+SELECT COUNT(*)
+FROM Repair r
+WHERE r.roomNo = givenRoomNo AND r.hotelID = givenHotelID AND r.repairDate BETWEEN r.repairDate AND DATEADD(month, 12, r.repairDate);
+
+// MITCHELL
+SELECT r.roomNo
+FROM Booking b, Room r
+WHERE b.hotelID = r.hotelID AND b.roomNo = r.roomNo AND b.bookingDate BETWEEN b.bookingDate AND DATEADD(day, 6, b.bookingDate);
+
+SELECT b.price
+FROM Booking b, Customer c
+WHERE c.customerID = b.customer AND c.fName = fNameVar AND c.lname = lNameVar
+LIMIT K
+ORDER BY price desc;
+
+DateRange *
+SELECT b.price
+FROM Booking b, Customer c
+WHERE c.customerID = b.customer AND c.fName = fNameVar AND c.lname = lNameVar AND b.hotelID = hotelIDVar;
+
+// List Top K Maintenance Company Names based on total repair count (descending order)
+SELECT m.name
+FROM MaintenanceCompany m, Repair r
+WHERE m.cmpID = r.mCompany
+LIMIT K
+ORDER BY (SELECT count (*)
+FROM MaintenanceCompany m, Repair r
+WHERE m.cmpID = r.mCompany) DESC;
+
+
 
 
